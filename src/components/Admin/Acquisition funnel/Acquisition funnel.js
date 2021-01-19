85ad38890dfb39e9ec21ef2fee60bb8b34d5de15
ruslanBik4 <bik4ruslan@gmail.com>
2021-01-19 13:29:37 +0200

    [MOD] form vacancy edit

--- src/components/Admin/Acquisition funnel/Acquisition funnel.js
+++ src/components/Admin/Acquisition funnel/Acquisition funnel.js
@@ -87,7 +87,9 @@ export default class AcquisitionFunnel extends Component {
   fetchRecruiters = async () => {
     const users = await getUsers();
 
-    if (users.staff !== undefined) {
+    if (users === 401) {
+      this.props.history.push('/login/')
+    } else if (users.staff !== undefined) {
       const recruiters = users.staff.filter((user) => user.id_roles === 2); // recruiter
       this.setState({recruiters});
     } else if (users.recruiters !== undefined) {
