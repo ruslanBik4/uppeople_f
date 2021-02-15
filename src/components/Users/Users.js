42a0dc6df8ba7cf5348569681775092d1d1990a3
ruslanBik4 <bik4ruslan@gmail.com>
2021-02-15 12:27:45 +0200

    [MOD] get companyes vacancies

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -94,7 +94,7 @@ export default class Users extends Component {
 
     const tabPanesForAdmin = Object.keys(users).map((key, idx) => {
       const index = (idx + 1).toString();
-      return users[key] !== undefined && users[key] !== NULL ? (
+      return users[key] !== undefined && users[key] !== null ? (
         <TabPane tabId={index} key={key}>
           <UsersTable
             users={users[key]}
